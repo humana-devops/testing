b1c2364307ba238c6afb410a4fc20d78e7939b6d
shameemabanu <57385528+shameemabanu@users.noreply.github.com>
2019-11-07 15:43:34 +0530

    Updated lines to 15

--- FloydTriangleExample.java
+++ FloydTriangleExample.java
@@ -8,7 +8,7 @@ class FloydTriangleExample
        Scanner input = new Scanner(System.in);
        System.out.println("Enter the number of rows for floyd's triangle:");
        //Copying user input into an integer variable named rows
-       rows = input.nextInt();
+       rows = 15;
        System.out.println("Floyd's triangle");
        System.out.println("****************");
        for ( counter = 1 ; counter <= rows ; counter++ )
